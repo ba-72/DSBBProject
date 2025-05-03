8bcfaf22fa1e05292d0fcede184b92178cb97e23
ba-72 <weihongcheng.work@foxmail.com>
2025-05-03 22:24:53 +0800

    加入部分Sobel梯度算法，时间复杂度过高

--- src/ImageGraph.java
+++ src/ImageGraph.java
@@ -7,8 +7,37 @@ public class ImageGraph {
     // 2. 计算链接成本（根据公式C(x,y) = 1/(1+G(x,y))）
     // 3. 构建8邻接图结构
 
+    int Gx=0;
+    int Gy=0;
+
+
+
     public ImageGraph(BufferedImage image) {
         // 计算Sobel梯度
+        int[][] grayPicture=new int[1024][768];//TODO:把int[][]的大小改为与导入图片外面加一圈防止数组溢出的数列之后的大小一致
+        //将图像化为灰度图并且转为矩阵保存在grayPicture
+        int[][] Sx={{-1,0,1},{-2,0,2},{-1,0,1}};
+        int[][] Sy={{-1,-2,-1},{0,0,0},{1,2,1}};
+        //TODO:这里的时间复杂度是O(n^4)，必须进行优化
+        for(int i=0;i<grayPicture.length;i++){
+            for (int j=0;j<grayPicture[i].length;j++){
+                for(int k=-1;k<=1;k++){
+                    for (int m=-1;m<=1;m++){
+                        Gx+=Sx[k][m]*grayPicture[i+k][j+m];
+                    }
+                }
+            }
+        }
+        //TODO:这也是
+        for(int i=0;i<grayPicture.length;i++){
+            for (int j=0;j<grayPicture[i].length;j++){
+                for(int k=-1;k<=1;k++){
+                    for (int m=-1;m<=1;m++){
+                        Gy+=Sy[k][m]*grayPicture[i+k][j+m];
+                    }
+                }
+            }
+        }
     }
 
     public double getCost(Point from, Point to) {
